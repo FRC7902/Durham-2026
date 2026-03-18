92214226cd8559f3b2b4f7bc2adab656f96ed37f
FRC7902-DS <driverstation@markhamfirebirds.ca>
2026-03-18 16:27:39 -0400

    feat: working right auto single sweep

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -191,18 +191,27 @@ public class Autos {
                 Commands.deadline(
                         driveToWaypoint(Position.NEUTRAL_RIGHT_1),
                         m_linearIntakeSubsystem.extend(),
-                        m_intakeRollerSubsystem.intake()),
-                
+                        m_intakeRollerSubsystem.intake(),
+                        m_indexerSubsystem.run()),
+
                 // Drive to center of field
                 driveToWaypoint(Position.NEUTRAL_RIGHT_2),
 
                 // Drive back to trench
                 Commands.deadline(
                         driveToWaypoint(Position.NEUTRAL_RIGHT_3),
-                        m_linearIntakeSubsystem.retract().andThen(m_intakeRollerSubsystem.stop())),
+                        m_linearIntakeSubsystem.retract().andThen(
+                                Commands.parallel(
+                                        m_intakeRollerSubsystem.stop(),
+                                        m_indexerSubsystem.stop()))),
 
                 // Get in shooting position
-                driveToWaypoint(Position.ALLIANCE_RIGHT_1));
+                driveToWaypoint(Position.ALLIANCE_RIGHT_1),
+
+                Commands.parallel(
+                        m_shooterSubsystem.aimAndShootIgnoreCheck(m_swerveSubsystem::getDistanceToTarget),
+                        m_indexerSubsystem.run(),
+                        m_intakeRollerSubsystem.intake()));
 
     }
 
